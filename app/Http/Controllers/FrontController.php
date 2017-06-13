3e1cbc2e59afc2b11a553ff2f229318fe01b6cc7
unknown <zmaliks2030@gmail.com>
2017-06-13 13:32:19 +0500

    second

--- app/Http/Controllers/FrontController.php
+++ app/Http/Controllers/FrontController.php
@@ -1,9 +1,5 @@
 <?php
-
 namespace App\Http\Controllers;
-
-
-
 use Illuminate\Http\Request;
 use App\Http\Requests;
 use Illuminate\Support\Facades\Auth;
@@ -14,52 +10,53 @@ use Gate;
 
 class FrontController extends Controller
 {
-    //
-    public function index(){
+    public function __construct(){
+        $this->middleware('check_login')->only('dashbaord');
+        $this->middleware('check_login')->only('logout');
+      
         
-         
+    }
+    
+    public function index(){
+        if(Auth::id()>0){
+             return redirect("front/dashboard");
+        }
         return view("front");
     }
     public function edit( $id){
-        
         $data_u=first_m::where("id",$id)->get();
-       
-        if($data_u){
-           
+         if($data_u){
             $users =  first_m::get();  
             $data['users']=$users;
-            $data['use']=$data_u;
+            $data['use']=$data_u;   
             return view("first",$data);
         }else{
             return redirect('first/');
         }
     }
     public function dashboard(){
-        
-            
-             return view("front_dash");
-       
-       
+         
+        return view("front_dash");
     }
     public function logout(){
-        echo Auth::id();
-         Auth::logout();
-         return redirect("front");
+        
+        Auth::logout();
+        return redirect("front");
     }
     public function add(Request $request){
        // print_r(Input::get('name'));
-      $user_m=new User();
-      $user_m->name=$request->name;
-      $user_m->email=$request->email;
-      $user_m->password=bcrypt($request->password);
-      $user_m->isAdmin="no";
-      $user_m->save();
+        $user_m=new User();
+        $user_m->name=$request->name;
+        $user_m->email=$request->email;
+        $user_m->password=bcrypt($request->password);
+        $user_m->isAdmin="no";
+        $user_m->save();
             return redirect('front/')->with('msg','Data Inserted Successfully');
        
     }
     public function login(Request $request){
         if (Auth::attempt(['email' => $request->email, 'password' => $request->password, 'isAdmin' => 'no'])) {
-                return redirect('front/dashboard');
+            return redirect('front/dashboard');
         }else{
             return redirect('front/')->with('msg','Try Again');
         }
